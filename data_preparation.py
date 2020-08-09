68196913e199bc51e96f52794745e1613c2981be
itayisraelov <israelovitay@gmail.com>
2020-08-09 17:27:50 +0300

    plot some graph

--- data_preparation.py
+++ data_preparation.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
-    df1 = pd.read_excel("2019_bronx.xlsx")
+    df1 = pd.read_excel("Data/2019/2019_bronx.xlsx")
     df1['AREA'] = 'bronx'
-    df2 = pd.read_excel("2019_brooklyn.xlsx")
+    df2 = pd.read_excel("Data/2019/2019_brooklyn.xlsx")
     df2['AREA'] = 'brooklyn'
-    df3 = pd.read_excel("2019_manhattan.xlsx")
+    df3 = pd.read_excel("Data/2019/2019_manhattan.xlsx")
     df3['AREA'] = 'manhattan'
-    df4 = pd.read_excel("2019_queens.xlsx")
+    df4 = pd.read_excel("Data/2019/2019_queens.xlsx")
     df4['AREA'] = 'queens'
-    df5 = pd.read_excel("2019_statenisland.xlsx")
+    df5 = pd.read_excel("Data/2019/2019_statenisland.xlsx")
     df5['AREA'] = 'statenisland'
 
     frames = [df1, df2, df3, df4, df5]
@@ -41,11 +42,38 @@ def main():
     df.drop(df.loc[df['SALE PRICE'] == 0].index, inplace=True)
     df = df.drop(['EASE-MENT', 'APARTMENT NUMBER'], axis=1)
 
+    # plot some graph
+    plot_some_graph(df)
 
 
+def plot_some_graph(df):
+    # area vs price
+    # gca stands for 'get current axis'
+    ax = plt.gca()
+    df.plot(kind='line', x='AREA', y='SALE PRICE', ax=ax)
+    # df.plot(kind='line',x='AREA',y='NEIGHBORHOOD',color='red', ax=ax)
+    plt.show()
 
+    # date vs number of transactions
+    df['SALE DATE'] = pd.to_datetime(df['SALE DATE'], infer_datetime_format=True)
+    plt.clf()
+    df['SALE DATE'].map(lambda d: d.month).plot(kind='hist')
+    plt.show()
 
+    # zip code vs price
+    ax = plt.gca()
+    df.plot(kind='line', x='ZIP CODE', y='SALE PRICE', ax=ax)
+    plt.show()
 
+    # block(tax)  vs price
+    ax = plt.gca()
+    df.plot(kind='line',x='BLOCK',y='SALE PRICE',ax=ax)
+    plt.show()
+
+    # total units vs price
+    ax = plt.gca()
+    df.plot(kind='line',x='TOTAL UNITS',y='SALE PRICE',ax=ax)
+    plt.show()
 
 
 if __name__ == '__main__':
